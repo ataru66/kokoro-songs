57c8ee5a14b60ccf8a638edc5ea977bf321fb194
Ataru Oniki <ataru19980606@gmail.com>
2020-05-28 15:39:10 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.i18n.fallbacks = true
   config.action_mailer.default_url_options = { :host => 'appname.herokuapp.com' }
 end
