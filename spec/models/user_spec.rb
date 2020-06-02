1eb47e66b530dbc957168e60453157a537f6c4b2
Ataru Oniki <ataru19980606@gmail.com>
2020-06-02 19:32:12 +0900

    Add context at do or do not
    contextを使用し保存できる場合とできない場合で分割しました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validates' do
     describe 'presence: true' do
+      context '保存できる場合' do
+      end
+      context '保存できない場合' do
+      end
     end
     describe 'uniqueness: true' do
+      context '保存できる場合' do
+      end
+      context '保存できない場合' do
+      end
     end
   end
 end
