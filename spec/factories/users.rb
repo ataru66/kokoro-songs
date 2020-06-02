e4021160bf42796a8473c2dff91f29968263e210
Ataru Oniki <ataru19980606@gmail.com>
2020-06-02 23:27:02 +0900

    update FactoryBot users.rb
    create_listなどで複数作成した時emailカラムが一意性制約で保存できないため、毎回異なるデータが入るようsequenceメソッドを使用しました。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ require 'faker'
 FactoryBot.define do
   factory :user do
     name { Faker::Games::Overwatch.hero }
-    email { Faker::Internet.email }
+    sequence(:email) { Faker::Internet.email }
     password { "12345678" }
     password_confirmation { "12345678" }
   end
