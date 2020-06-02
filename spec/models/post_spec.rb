362e4970ea5b50b335b31117efa85b6842cacf60
Ataru Oniki <ataru19980606@gmail.com>
2020-06-02 14:04:47 +0900

    Add presence:true test at each columns
    バリデーションのpresenceが正常に機能し保存できない場合のテストコードを記述しました。
    それぞれ対象のカラムの値を空の文字列を代入しvalid?メソッドでバリデーションにひっかかることを確認しています。

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,6 +9,35 @@ RSpec.describe Post, type: :model do
 
     context '保存できない場合' do
 
+      it 'contentカラムが空だと保存できないこと' do
+        @post.content = ""
+        @post.valid?
+        expect(@post.errors[:content]).to include("can't be blank")
+      end
+
+      it 'artist_idカラムが空だと保存できないこと' do
+        @post.artist_id = ""
+        @post.valid?
+        expect(@post.errors[:artist_id]).to include("can't be blank")
+      end
+
+      it 'song_idカラムが空だと保存できないこと' do
+        @post.song_id = ""
+        @post.valid?
+        expect(@post.errors[:song_id]).to include("can't be blank")
+      end
+
+      it 'artistカラムが空だと保存できないこと' do
+        @post.artist = ""
+        @post.valid?
+        expect(@post.errors[:artist]).to include("can't be blank")
+      end
+
+      it 'songカラムが空だと保存できないこと' do
+        @post.song = ""
+        @post.valid?
+        expect(@post.errors[:song]).to include("can't be blank")
+      end
     end
 
     context '保存できる場合' do
