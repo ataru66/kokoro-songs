c01d7ee3483cc279b8cb5a405de1b2594508dc1c
Ataru Oniki <ataru19980606@gmail.com>
2020-06-02 23:31:45 +0900

    add posts#index test code
    postsコントローラーのindexアクションをテストするコードを記述しました。

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 describe PostsController do
   describe 'GET #index' do
-  
+    it '@postsの中身が期待通りであること' do
+      posts = create_list(:post, 5)
+      get :index
+      expect(assigns(:posts)).to match(posts.sort{|a, b| b.updated_at <=> a.updated_at})
+    end
+    it 'posts/index.html.hamlに遷移すること' do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 end
