f810c47b09ab2efc11f675331f3296c7127b051e
Ataru Oniki <ataru19980606@gmail.com>
2020-06-02 13:59:26 +0900

    add before post_spec.rb
    全テストで共通して使用するPostモデルとUserモデルのインスタンスをbeforeで生成しました。

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe 'validates' do
+    before do
+      @user = create(:user)
+      @post = build(:post)
+    end
 
     context '保存できない場合' do
 
