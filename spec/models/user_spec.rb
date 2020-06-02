7f2fcdea220c0521963d1e2a1ffa7e60abd4a807
Ataru Oniki <ataru19980606@gmail.com>
2020-06-02 19:30:57 +0900

    Add describe for validates
    バリデーションのテストを記述するブロックを作成し、さらにpresence と uniqueness に分けました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'validates' do
+    describe 'presence: true' do
+    end
+    describe 'uniqueness: true' do
+    end
+  end
 end
