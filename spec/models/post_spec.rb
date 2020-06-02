494fedce2fb24d988939871c113096d1ff50a587
Ataru Oniki <ataru19980606@gmail.com>
2020-06-02 13:58:10 +0900

    add context
    Postモデルの単体テストdescribe "validates"の中に
    保存できる場合とできない場合をcontextを使用して分岐させました。

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe 'validates' do
+
+    context '保存できない場合' do
+
+    end
+
+    context '保存できる場合' do
+
+    end
   end
 
 end
