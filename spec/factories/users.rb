574a72857ad73e17857383863a8029e5b1abfb37
Ataru Oniki <ataru19980606@gmail.com>
2020-06-11 14:49:28 +0900

    Update users.rb
    一意性制約に引っかかってしまうため設定を変更しました。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryBot.define do
   factory :user do
-    name { Faker::Games::Overwatch.hero }
+    name { Faker::Name.name }
     sequence(:email) { Faker::Internet.email }
     password { "12345678" }
     password_confirmation { "12345678" }
