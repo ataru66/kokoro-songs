23d0b7c08692a1b9bf75072b8098a712d84fb44c
Ataru Oniki <ataru19980606@gmail.com>
2020-06-16 17:43:40 +0900

    Update posts.rb
    newアクションで変数の値を統一するために変更しました。

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,8 +3,8 @@ require 'faker'
 FactoryBot.define do
   factory :post do
     content { Faker::Games::Zelda.item }
-    artist_id { "000000" }
-    song_id { "999999" }
+    artist_id { "5Vo1hnCRmCM6M4thZCInCj" }
+    song_id { "58dxGXavrcagRqA58fNB0Z" }
     artist { Faker::Music.band }
     song { Faker::Game.title }
     updated_at { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
