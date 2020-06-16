64fed30b5a08d48964c79f5a93084fe9200f2b11
Ataru Oniki <ataru19980606@gmail.com>
2020-06-16 17:46:54 +0900

    add test code of posts#new
    postsコントローラーnewアクションのテストコードを記述しました。

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,4 +12,31 @@ describe PostsController do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #new' do
+    before do
+      @params = { song_id: "58dxGXavrcagRqA58fNB0Z", artist_id: "5Vo1hnCRmCM6M4thZCInCj" }
+      @song = RSpotify::Track.find(@params[:song_id])
+      @artist = @song.artists[0]
+      @release_year = @song.album.release_date.gsub(/-.+/, "")
+      @posts = create_list(:post, 3)
+      get :new, params: @params
+    end
+    it '@songの中身が期待通りであること' do
+      expect(assigns(:song)).to have_attributes(id: @song.id)
+    end
+    it '@artistの中身が期待通りであること' do
+      expect(assigns(:artist)).to have_attributes(id: @artist.id)
+    end
+    it '@release_yearの中身が期待通りであること' do
+      expect(assigns(:release_year)).to match(@release_year)
+    end
+    it '@postsの中身が期待通りであること' do
+      @posts = select_posts(@posts)
+      expect(assigns(:posts)).to match(sort_DESC(@posts))
+    end
+    it 'posts/new.html.hamlに遷移すること' do
+      expect(response).to render_template :new
+    end
+  end
 end
