8b1c5de2f3463175e99336a671aef163e7780c02
Ataru Oniki <ataru19980606@gmail.com>
2020-06-16 17:50:13 +0900

    update posts#index test code
    newアクションで使用したメソッドをこちらにも流用しました。

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -15,7 +15,7 @@ describe PostsController do
     it '@postsの中身が期待通りであること' do
       posts = create_list(:post, 5)
       get :index
-      expect(assigns(:posts)).to match(posts.sort{|a, b| b.updated_at <=> a.updated_at})
+      expect(assigns(:posts)).to match(sort_DESC(posts))
     end
     it 'posts/index.html.hamlに遷移すること' do
       get :index
