ccb5c3f42528b583b1b8cc0d1602793627608bbe
Ataru Oniki <ataru19980606@gmail.com>
2020-06-16 12:34:43 +0900

    add authenticate_user! in posts_controller
    投稿・編集・削除のみログインする必要があるため、postsコントローラーにauthenticate_user!をbefore_actionで設定しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
   before_action :set_post, only: [:edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
