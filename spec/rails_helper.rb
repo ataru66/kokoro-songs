31bbf627a431d7cb24e133c1953edf788a5fe8df
Ataru Oniki <ataru19980606@gmail.com>
2020-06-17 15:11:18 +0900

    add devise config to spec/rails_helper.rb
    rails_helperにdeviseをテストで使用するための設定をRspecのgithubの記述を元に追記、削除しました。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,8 +64,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::ControllerHelpers, type: :view
-  config.include Devise::TestHelpers, type: :controller
   config.include ControllerMacros, type: :controller
 end
