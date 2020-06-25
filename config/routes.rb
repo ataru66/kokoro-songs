92e851f5356a0dcad1ad5289061bb9253c19f0b3
Ataru Oniki <ataru19980606@gmail.com>
2020-06-25 21:05:21 +0900

    Update routes.rb
    新しくalbumでの検索をかけられるようルーティングを追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,20 @@ Rails.application.routes.draw do
   get "musics/select/artist", to: "musics#select_artist"
   get "musics/search/songs", to: "musics#search_songs"
   get "musics/search/only/songs", to: "musics#search_only_songs"
+
+  resources :artists, only: :index
+  scope :artists do
+    get "/search", to: "artists#search", as: :artists_search
+    get "/select", to: "artists#select", as: :artist_select
+  end
+  resources :albums, only: :index
+  scope :albums do
+    get "/search", to: "album#search", as: :albums_search
+    get "/select", to: "album#select", as: :album_select
+  end
+  resources :tracks, only: :index
+  scope :tracks do
+    get "/search", to: "tracks#search", as: :tracks_search
+    get "/select", to: "tracks#select", as: :track_select
+  end
 end
