e43de17443e368df156d1208582892b5e4a33a46
Ataru Oniki <ataru19980606@gmail.com>
2020-06-25 21:16:50 +0900

    add artists#search action
    artists#searchアクションを定義しました。
    ヘッダーのアイコンからクリックするhtmlでのレスポンス時search.html.hamlを返し、form入力時はjQueryを利用してjson形式で返されるように設定しました。

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,13 @@
 class ArtistsController < ApplicationController
-  
+
+  def search
+    if params[:search].present?
+      @artists = RSpotify::Artist.search(params[:search], limit: 50, market: 'JP')
+    end
+    respond_to do |format|
+      format.html
+      format.json { render json: @artists }
+    end
+  end
+
 end
