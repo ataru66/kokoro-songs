1947bdd66474f64c02874b3fc875ce6dc07125af
Ataru Oniki <ataru19980606@gmail.com>
2020-06-30 04:06:20 +0900

    add convert_mins to trackshelper
    tracksヘルパーにconvert_minsを定義しました。
    ミリ秒を引数に渡し、分秒で返してくれるメソッドです。

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,13 @@
 module TracksHelper
+  def convert_mins(milliseconds)
+    return '' unless milliseconds
+    hours, milliseconds   = milliseconds.divmod(1000 * 60 * 60)
+    minutes, milliseconds = milliseconds.divmod(1000 * 60)
+    seconds, milliseconds = milliseconds.divmod(1000)
+    if seconds.to_s.length == 1
+      "#{minutes}:0#{seconds}"
+    else
+      "#{minutes}:#{seconds}"
+    end
+  end
 end
