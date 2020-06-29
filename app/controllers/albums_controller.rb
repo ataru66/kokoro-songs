43a5b5cf470505a34acee0e355ae032577c08467
Ataru Oniki <ataru19980606@gmail.com>
2020-06-30 04:20:47 +0900

    add albums#show
    albums#showを定義しました。
    選択されたアーティストとアルバムとアルバムに収録されている曲をインスタンス変数で定義しました。

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,7 @@
 class AlbumsController < ApplicationController
+  def show
+    @artist = RSpotify::Artist.find(params[:artist_id])
+    @album = RSpotify::Album.find(params[:id])
+    @tracks = @album.tracks
+  end
 end
