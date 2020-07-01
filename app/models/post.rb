e542363346b9bbae66be907f91000fa05c67a27e
Ataru Oniki <ataru19980606@gmail.com>
2020-07-01 21:35:43 +0900

    Update post.rb
    DB設計変更に伴いバリデーションとアソシエーションを変更しました。

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ApplicationRecord
   validates :content, presence: true
-  validates :artist_id, presence: true
-  validates :song_id, presence: true
-  validates :artist, presence: true
-  validates :song, presence: true
+  validates :user, presence: true
+  validates :album_id, presence: true
+  validates :track_id, presence: true
+
   belongs_to :user
+  has_many :artists, through: :artists_posts
+  has_many :artists_posts
+  belongs_to :album
+  belongs_to :track
 end
