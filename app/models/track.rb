16bc2a1f10c95114f22d04cf36ef521abad19d2b
Ataru Oniki <ataru19980606@gmail.com>
2020-07-16 17:52:41 +0900

    Update track.rb
    track.rbにartists_tracksテーブルとのアソシエーションを定義しました。

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,6 +3,7 @@ class Track < ApplicationRecord
   validates :album_id, presence: true
 
   has_many :posts
-  has_many :artists
+  has_many :artists, through: :artists_tracks
+  has_many :artists_tracks
   belongs_to :album
 end
