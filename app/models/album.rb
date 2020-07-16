4ca01e2d51d224b387a094075eb8c203ba93ca41
Ataru Oniki <ataru19980606@gmail.com>
2020-07-16 17:49:23 +0900

    add association to album
    albumモデルにartists_albumsテーブルと結びつけるアソシエーションを定義しました。

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,7 @@ class Album < ApplicationRecord
   validates :name, presence: true
 
   has_many :posts
-  has_many :artists
+  has_many :artists, through: :artists_albums
+  has_many :artists_albums
   has_many :tracks
 end
