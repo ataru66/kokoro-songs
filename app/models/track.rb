cfb9aaa6dbc4fa2de2210c7a3ca6908a200a7ff0
Ataru Oniki <ataru19980606@gmail.com>
2020-07-20 21:59:15 +0900

    Update track.rb
    アソシエーションの記述順によりエラーが発生したため修正しました。

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,7 +3,7 @@ class Track < ApplicationRecord
   validates :album_id, presence: true
 
   has_many :posts
-  has_many :artists, through: :artists_tracks
   has_many :artists_tracks
+  has_many :artists, through: :artists_tracks
   belongs_to :album
 end
