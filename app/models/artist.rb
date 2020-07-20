ca021f493aa55d8cdcc580d9d965c5101512096e
Ataru Oniki <ataru19980606@gmail.com>
2020-07-20 21:58:55 +0900

    Update artist.rb
    アソシエーションの記述順によりエラーが発生したため修正しました。

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,10 @@
 class Artist < ApplicationRecord
   validates :name, presence: true
 
-  has_many :posts, through: :artists_posts
   has_many :artists_posts
-  has_many :albums, through: :artists_albums
+  has_many :posts, through: :artists_posts
   has_many :artists_albums
-  has_many :tracks, through: :artists_tracks
+  has_many :albums, through: :artists_albums
   has_many :artists_tracks
+  has_many :tracks, through: :artists_tracks
 end
