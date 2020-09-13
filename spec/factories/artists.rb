af91bf1c1ad71614d52dbd12f86d534cdfddd28b
Ataru Oniki <ataru19980606@gmail.com>
2020-09-13 22:43:39 +0900

    Update artists.rb
    データベース設計変更に伴い、FactoryBot使用時のテスト用データを作成しました。

--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :artist do
-    
+    id { "5Vo1hnCRmCM6M4thZCInCj" }
+    name { "Official HIGE DANdism" }
+    image { "https://i.scdn.co/image/981604b4d2bcbf83f228d0b303582feac7b7ae4a" }
   end
 end
