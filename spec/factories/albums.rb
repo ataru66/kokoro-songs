cdc9afb53c0944c5251dc8837b2ca8cd11908133
Ataru Oniki <ataru19980606@gmail.com>
2020-09-13 22:43:32 +0900

    Update albums.rb
    データベース設計変更に伴い、FactoryBot使用時のテスト用データを作成しました。

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :album do
-    
+    id { "0zoEfi1Tq2OXlhD2cSClWW" }
+    name { "Laughter" }
+    image { "https://i.scdn.co/image/ab67616d0000b273e3487c07d29e73d46211ac73" }
+    release_date { "2020-07-10" }
   end
 end
