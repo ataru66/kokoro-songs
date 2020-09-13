4d5dba52ff4ccdc6f4be831503d9647a0dd125c9
Ataru Oniki <ataru19980606@gmail.com>
2020-09-13 22:42:53 +0900

    Update posts.rb
    データベース設計変更に伴い、テスト用データを追加しました。

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,6 +3,8 @@ require 'faker'
 FactoryBot.define do
   factory :post do
     content { Faker::Games::Zelda.item }
+    album_id { "0zoEfi1Tq2OXlhD2cSClWW" }
+    track_id { "1SdwlHsv8bWHbAlIw5Jfgo" }
     updated_at { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
     user
   end
