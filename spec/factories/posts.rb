3513dd522acfe5512c03b5ef7cce69a22ab533ba
Ataru Oniki <ataru19980606@gmail.com>
2020-09-13 22:42:20 +0900

    Update posts.rb
    データベース設計変更に伴い不要なテスト用データを削除しました。

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,10 +3,6 @@ require 'faker'
 FactoryBot.define do
   factory :post do
     content { Faker::Games::Zelda.item }
-    artist_id { "5Vo1hnCRmCM6M4thZCInCj" }
-    song_id { "58dxGXavrcagRqA58fNB0Z" }
-    artist { Faker::Music.band }
-    song { Faker::Game.title }
     updated_at { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
     user
   end
